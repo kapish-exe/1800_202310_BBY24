3b6e6626de9daa93bd64941370113dd2962fc2d2
gurpinder2023 <gurpinderk714@gmail.com>
2023-03-10 10:53:12 -0800

    added data to profile.html

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -9,7 +9,8 @@ var uiConfig = {
           db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                  username: user.displayName,                    //"users" collection
                  email: user.email,                         //with authenticated user's ID (user.uid)
-                 country: "Canada",                      //optional default profile info      
+                 country: "Canada",
+                                      //optional default profile info      
                                          
           }).then(function () {
                  console.log("New user added to firestore");
@@ -47,3 +48,5 @@ var uiConfig = {
   };
 
   ui.start('#firebaseui-auth-container', uiConfig);
+
+
