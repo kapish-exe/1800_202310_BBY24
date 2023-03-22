f8bfb22e750d813297c090a476f9e1dc98193003
Emily0816Lin <emilysmileee@gmail.com>
2023-03-22 00:37:00 -0700

    fixed store DB problem

--- scripts/customize.js
+++ scripts/customize.js
@@ -1,30 +1,25 @@
 function submitCustomize() {
-    console.log("inside write review")
-    let UserSelectPeoNum = document.getElementById('selectPeoNum').value;
-    let UserSelectHr = document.getElementById('selectHr').value;
-    let UserSelectType = document.querySelector('input[name="selectType"]:checked').value;
-    console.log(userSelectPeoNum, userSelectHr, userSelectType);
+    var selectPeoNum = document.getElementById('selectPeoNum').value;
+    var selectHr = document.getElementById('selectHr').value;
+    var selectType = document.querySelector('input[name="selectType"]:checked').value
+    console.log(selectPeoNum, selectHr, selectType);
 
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            var currentUser = db.collection("users").doc(user.uid)
-            var userID = user.uid;
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    var userEmail = userDoc.data().email;
-                    db.collection("customizedKit").add({
-                        userID: userID,
-                        numberOfPeople: UserSelectPeoNum,
-                        numberOfHr: UserSelectHr,
-                        typeOfKit: UserSelectType,
-                    }).then(() => {
-                        window.location.href = "emergencyKit.html"; //new line added
-                    })
-                })
+            db.collection("customizeKit").doc(user.uid).set({
+                userID: user.uid,
+                selectPeoNum: selectPeoNum,
+                selectHr: selectHr,
+                selectType: selectType,
+            }).then(() => {
+                console.log("Customized kit created");
+                // window.location.href = "emergencyKit.html";
+            }).catch((error) => {
+                console.error("Error creating customized kit:", error);
+            });
         } else {
             console.log("No user is signed in");
-            window.location.href = 'customizedKit.html';
         }
     });
 }
+
